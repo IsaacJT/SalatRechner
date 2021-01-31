cf6cab7b2e2603b8f410e1044336974fa6f278c4
Isaac True <isaac@is.having.coffee>
2021-01-31 15:53:29 +0100

    Switched to typescript-based project

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Home />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Home from '@/components/Home.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Home,
+  },
+});
+</script>
